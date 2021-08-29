f417b72d639cf06747d0198112f34c87375de108
Davide <davide.giacometti@outlook.it>
2021-08-29 14:55:55 +0200

    update additional options for launcher plugins

--- src/modules/launcher/PowerLauncher/SettingsReader.cs
+++ src/modules/launcher/PowerLauncher/SettingsReader.cs
@@ -2,7 +2,6 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Abstractions;
@@ -84,6 +83,8 @@ namespace PowerLauncher
                     {
                         Log.Info($"Successfully read new settings. retryCount={retryCount}", GetType());
                     }
+
+                    UpdateAdditionalOptionsSettings(overloadSettings);
 
                     if (overloadSettings.Plugins == null || overloadSettings.Plugins.Count() != PluginManager.AllPlugins.Count)
                     {
@@ -214,6 +215,38 @@ namespace PowerLauncher
                 IconPathLight = GetIcon(x.Metadata, x.Metadata.IcoPathLight),
                 AdditionalOptions = x.Plugin is ISettingProvider ? (x.Plugin as ISettingProvider).AdditionalOptions : new List<PluginAdditionalOption>(),
             });
+        }
+
+        private void UpdateAdditionalOptionsSettings(PowerLauncherSettings settings)
+        {
+            foreach (var pluginPair in PluginManager.AllPlugins)
+            {
+                var plugin = pluginPair.Plugin;
+
+                if (plugin is ISettingProvider && (plugin as ISettingProvider).AdditionalOptions != null)
+                {
+                    var pluginSettings = settings.Plugins.Single(p => p.Id == pluginPair.Metadata.ID);
+
+                    foreach (var option in (plugin as ISettingProvider).AdditionalOptions)
+                    {
+                        var pluginSetting = pluginSettings.AdditionalOptions.SingleOrDefault(o => o.Key == option.Key);
+
+                        if (pluginSetting == null)
+                        {
+                            pluginSettings.AdditionalOptions = pluginSettings.AdditionalOptions.Append(option);
+                        }
+                        else
+                        {
+                            pluginSetting.DisplayLabel = option.DisplayLabel;
+                        }
+                    }
+
+                    var optionsToDelete = pluginSettings.AdditionalOptions.Except((plugin as ISettingProvider).AdditionalOptions);
+                    pluginSettings.AdditionalOptions = pluginSettings.AdditionalOptions.Except(optionsToDelete);
+                }
+
+                _settingsUtils.SaveSettings(settings.ToJsonString(), PowerLauncherSettings.ModuleName);
+            }
         }
     }
 }
--- src/settings-ui/Microsoft.PowerToys.Settings.UI.Library/PluginAdditionalOption.cs
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI.Library/PluginAdditionalOption.cs
@@ -2,14 +2,29 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.Collections.Generic;
+
 namespace Microsoft.PowerToys.Settings.UI.Library
 {
-    public class PluginAdditionalOption
+    public class PluginAdditionalOption : IEquatable<PluginAdditionalOption>
     {
         public string Key { get; set; }
 
         public string DisplayLabel { get; set; }
 
         public bool Value { get; set; }
+
+        public bool Equals(PluginAdditionalOption other)
+        {
+            return Key == other?.Key;
+        }
+
+        public override int GetHashCode()
+        {
+            return 990326508 + EqualityComparer<string>.Default.GetHashCode(Key);
+        }
+
+        public override bool Equals(object obj) => Equals(obj as PluginAdditionalOption);
     }
 }
